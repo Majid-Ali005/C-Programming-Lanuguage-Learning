d04da4daf930a489dbb33a76aff845849088e8f4
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-03 15:43:55 +0000

    the c first program with output

---
+++ chpater 1/first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world \n"); // the \n is used for to create new line
+         printf("My Age is %d", 25);
+    // this is the another way for outputs in c
+        puts("hello world \n");
+    puts("My Age is");
+
+
+printf("hello world");
+
+
+
+    return 0;
+}
