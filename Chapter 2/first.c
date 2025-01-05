8c0887cb6814d053dc740e07b07fcd5707e9f924
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-05 06:37:32 +0000

    Create a program that takes two numbers and shows result of all arithmetic operators (+,-,*,/,%).

---
+++ Chapter 2/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 3.14;
+    printf("%d",a);
+
+
+    
+    return 0;
+}
---
+++ Chapter 2/problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int val1 = 10;
+    int val2 = 20;
+    int mul = val1*val2;
+    int div = val1/val2;
+    int sub = val1-val2;
+    int add = val1+val2;
+    printf("multiplica is %d \n", mul);
+    printf("division is %d \n", div);
+    printf("subtraction is %d \n", sub);
+    printf("addition is %d", add);
+
+    return 0;
+}
