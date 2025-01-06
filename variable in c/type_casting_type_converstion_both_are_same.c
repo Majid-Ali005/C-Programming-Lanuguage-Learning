4e3c8df6986c79b6b538e7b8b3b3cbbec5e8b500
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-06 18:25:34 +0000

    type casting

---
+++ variable in c/type_casting_type_converstion_both_are_same.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    double x = 1.2;
+    int sum = (int) x +1;
+    printf("%d",sum); //output: 2
+    
+
+    //2nd example
+     float a = 1.5;
+  int b = (int)a;
+
+  printf("a = %f\n", a);
+  printf("b = %d\n", b);
+  return 0;
+
+}
