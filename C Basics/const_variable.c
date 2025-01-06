bc91609a2c2bfb8ffd7afa607d579be327c8bbf6
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-06 15:53:33 +0000

    const

---
+++ C Basics/const_variable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // bhai ye constant nhi hy q k iss me const nhi laga hy.
+    int not_constant;
+
+    // constant variable; ye hy q k const laga hy.
+    const int constant = 20;
+
+    // changing values
+    not_constant = 40;
+    // constant = 22;
+    const int x;
+    x = 10;
+
+
+
+    return 0;
+}
