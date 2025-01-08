7eb797a4619532d172236c4d0335a01feaeb8487
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-08 06:05:36 +0000

    binary arithmetic operator

---
+++ operators/first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    const int a = 5;
+    const int b = 5;
+    printf("Addition %d\n", a+b);
+    printf("Subtraction %d\n", a+b);
+    printf("Multiply %d\n", a*b);
+    printf("Devide %d\n", a/b);
+    printf("Modulus %d", a%b);
+    return 0;
+}
+
