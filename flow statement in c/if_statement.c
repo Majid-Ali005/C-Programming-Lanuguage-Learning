afa423e398504f94e8674fa24f821a16d05097ac
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-09 06:30:22 +0000

    Negative Positive Number Program

--- flow statement in c/if_statement.c
+++ flow statement in c/if_statement.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+    //iss ko khud se papaer pr explain kare.
+    //Majid Ali
     int n = 30;
     if(n<10)
     {
@@ -9,7 +11,7 @@ int main()
     }
     if(n > 20)
     {
-        printf("%d < 20", n);
+        printf("%d < 20", n); // 30 < 20
     }
     return 0;
 }
---
+++ flow statement in c/negative_positive_number_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = -4;
+    if(num > 0)
+    {
+        printf("Number is positive %d",num);
+    }
+    if(num < 0)
+    {
+        printf("Number is Negative %d", num);
+        //Output: Number is Negative
+    }
+    return 0;
+}
+
