c2d42b7139a17e6eee02b9557e02aca577a60887
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-09 06:12:23 +0000

    this is the simple greater and less than program with if statement.

---
+++ operators/flow statement in c/if_statement.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 30;
+    if(n<10)
+    {
+        printf("%d < 10", n);
+    }
+    if(n > 20)
+    {
+        printf("%d < 20", n);
+    }
+    return 0;
+}
