e91de864d26b592df10b036d6b4c04a726ab2313
M. Majid Ali <147548345+Majid-Ali005@users.noreply.github.com>
2025-01-09 06:35:36 +0000

    number lies in range program

---
+++ flow statement in c/Number_Lies_program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    //check number wether range in between 10,20
+    int n = 11;
+    if(n >= 10 && n<=20)
+    {
+        printf("%d number lies between [10,20]",n);
+    }
+    return 0;
+}
